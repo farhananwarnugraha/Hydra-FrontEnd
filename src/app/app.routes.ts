1e1405c38c89ea423122278681e04fd0a570cd0b
farhananwarnugraha <nugraha21official@gmail.com>
2024-10-21 17:28:11 +0700

    Make Guest  Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
---
+++ src/app/guest/dashboard-guest/dashboard-guest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-guest',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-guest.component.html',
+  styleUrl: './dashboard-guest.component.css'
+})
+export class DashboardGuestComponent {
+
+}
