4f8aa8e6523d48f6a95ccb165fd3fc50838f3783
farhananwarnugraha <nugraha21official@gmail.com>
2024-10-31 17:28:34 +0700

    fixing view Add Customer

--- src/app/admin/admin-candidate/candidate-form/candidate-form.component.ts
+++ src/app/admin/admin-candidate/candidate-form/candidate-form.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-candidate-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './candidate-form.component.html',
   styleUrl: './candidate-form.component.css'
 })
