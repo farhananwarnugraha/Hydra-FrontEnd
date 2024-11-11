e68e8ed2411da6632501e17a2c722bd222872aef
farhananwarnugraha <nugraha21official@gmail.com>
2024-11-11 11:18:42 +0700

    fixing candidat TrainingManager

--- src/app/admin/admin-candidate/candidate-evaluation-list/candidate-evaluation-list.component.ts
+++ src/app/admin/admin-candidate/candidate-evaluation-list/candidate-evaluation-list.component.ts
@@ -2,27 +2,39 @@ import { Component, inject, OnInit } from '@angular/core';
 import { TableLayoutComponent } from '../../../shared/table-layout/table-layout.component';
 import { EvauationResult } from '../evaluation-result.model';
 import { EvaluationResultService } from '../evaluation-result.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CandidateEvaluationComponent } from '../candidate-evaluation/candidate-evaluation.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-candidate-evaluation-list',
   standalone: true,
-  imports: [TableLayoutComponent, CandidateEvaluationComponent],
+  imports: [TableLayoutComponent, CandidateEvaluationComponent, ReactiveFormsModule, RouterLink],
   templateUrl: './candidate-evaluation-list.component.html',
   styleUrl: './candidate-evaluation-list.component.css'
 })
 export class CandidateEvaluationListComponent implements OnInit{
   evaluations: EvauationResult[] = [];
   totalPage!: number;
+  pageSize!: number;
   isLoading: boolean = true;
+
+  evaluationFilterForm = new FormGroup({
+    candidateName: new FormControl(''),
+    pageNumber: new FormControl(1),
+    pageSize: new FormControl(10)
+  });
 
   private _evaluationResultService = inject(EvaluationResultService);
   private _route = inject(ActivatedRoute);
+  private _router = inject(Router);
 
   ngOnInit(): void {
-    this.loadEvaluationData();
+    this._loadingEvaluationWithParams();
+    this._fiterChange();
   }
+
 
   loadEvaluationData(){
     this.isLoading = true;
@@ -34,9 +46,46 @@ export class CandidateEvaluationListComponent implements OnInit{
     })
   }
 
-  // private _loadingEvaluationWithParams(){
-  //   this._route.queryParams.subscribe((params) => {
-  //     this.
-  //   })
-  // }
+  private _loadingEvaluationWithParams(){
+    this._route.queryParams.subscribe((params) => {
+      this.evaluationFilterForm.patchValue({
+        candidateName: params['candidateName'] || null,
+        pageNumber: params['pageNumber'] || 1,
+        pageSize: params['pageSize'] || 10
+      }, {emitEvent: false});
+      this.loadEvaluationData();
+    })
+  }
+
+  private _fiterChange(){
+    this.evaluationFilterForm.valueChanges
+    .pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      tap((valueForm) =>{
+        const queryParam = {
+          fullName: valueForm.candidateName?.trim() || null,
+          pageNumber: valueForm.pageNumber || 1,
+          pageSize: valueForm.pageSize || 10
+        };
+
+        this._router.navigate([],{
+          relativeTo: this._route,
+          queryParams: queryParam,
+          queryParamsHandling: 'merge'
+        });
+      }),
+      switchMap((valueForm) => {
+        const filter = {
+          fullName: valueForm.candidateName?.trim() || null,
+          pageNumber: valueForm.pageNumber!,
+          pageSize: valueForm.pageSize!
+        };
+        return this._evaluationResultService.getAllEvaluationResult(filter);
+      })
+    ).subscribe((result) => {
+      this.evaluations = result.data.evaluationCandidates;
+      this.totalPage = result.data.paginations.totalPage;
+    })
+  }
 }
